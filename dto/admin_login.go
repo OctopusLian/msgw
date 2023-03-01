64bd9fa9ee71a5d5bb6d8dcdca8c1bdfbc3e4ff6
OctopusLian <zoctopus@qq.com>
2023-03-01 23:02:08 +0800

    perf(admin_login): username参数校验

--- dto/admin_login.go
+++ dto/admin_login.go
@@ -13,8 +13,8 @@ type AdminSessionInfo struct {
 }
 
 type AdminLoginInput struct {
-	UserName string `json:"username" form:"username" comment:"管理员用户名" example:"admin" validate:"required,valid_username"` //管理员用户名
-	Password string `json:"password" form:"password" comment:"密码" example:"123456" validate:"required"`                   //密码
+	UserName string `json:"username" form:"username" comment:"管理员用户名" example:"admin" validate:"required,is_valid_username"` //管理员用户名
+	Password string `json:"password" form:"password" comment:"密码" example:"123456" validate:"required"`                      //密码
 }
 
 func (param *AdminLoginInput) BindValidParam(c *gin.Context) error {
